7083ad54700467c4cd9013afd2365882c9ee0704
Gabriel Goulart <goulartggabriel@gmail.com>
2023-11-01 15:49:18 -0300

    Prefabs updates

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -7,7 +7,7 @@ public class LevelLoader : MonoBehaviour {
     #region Variables
 
     public Animator transition;
-    public float transitionTime = 12f;
+    public float transitionTime = 10f;
     public int currentBuildIndex = 0;
     private int lastSceneBuildIndex = 0;
     private int levelCount = 15;
